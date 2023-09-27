50bef3b96a145552a4d4e977f5622b0917df705c
LauritaR <laurita.radisevska@mif.stud.vu.lt>
2023-09-28 01:34:53 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,26 +1,120 @@
-#include "functions.cpp" 
+#include "test.cpp" 
+
+int user_input_request()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    int ans;
+    char yn, ni;
+    
+    cout<<"-----Hash generatorius-----\n";
+    cout<<endl;
+    cout<<"Pasirinkite norimą opciją:   \n";
+    cout<<endl;
+    cout<<"1. Generuoti hash'ą jūsų tekstui.\n";
+    cout<<"2. Testavimas.\n";
+    cin>>ans;
+    cout<<"Pasirinkote opciją: "<<ans<<"\n";
+    cout<<"Ar norite tęsti?(y/n)  \n";
+    cin>>yn;
+    while(true)
+    {
+        if(yn=='y')
+        {
+          return ans;
+        }
+        else{
+            cout<<"Paleist programą iš naujo(n) ar išeti(i)?(n/i)";
+            cin>>ni;
+            if(ni=='n')
+              user_input_request();
+            else {
+                cout<<"Išeinama iš programos...";
+                exit(0);
+            }
+        } 
+    }
+}
+
+void openf_or_manual()
+{
+    char ans;
+    string ans2;
+    SetConsoleOutputCP(CP_UTF8);
+    cout<<"Generuoti hash'ą iš failo(f) ar norite įvesti tekstą rankiniu būdu?(r)?(f/r) \n";
+    cin>>ans;
+    if(ans=='f')
+    {
+        cout<<"Parašykite failo pavadinimą (.txt formatu):  \n";
+        cin>>ans2;
+        stringstream file_str;
+        ifstream UserFile(ans2);
+        file_str<<UserFile.rdbuf();
+        string input=file_str.str();
+        string output=hashed(input);
+        cout<<output<<endl;    
+    }
+    else if(ans=='r')
+    {
+        cout<<"Parašykite tekstą:  ";
+        cin>>ans2;
+        string output=hashed(ans2);
+        cout<<output<<endl;
+    }
+} 
+
+
+void generateF(string filename, int num)
+{  
+    srand(static_cast<unsigned int>(time(NULL)));
+    RandInt rnd(0, num);
+    ofstream genr(filename); 
+  
+    if (!genr.is_open()) {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return;
+    }
+    string str="";
+   for(int i=0;i<num;i++)
+    {
+        int randIndx=rnd();
+        str.push_back(symbols[randIndx%(sizeof(symbols)-1)]);
+        randIndx++;
+    } 
+
+    genr<<str;
+    genr.close();
+
+}
+
+void test_option()
+{
+   int pageidavimas;
+    cout<<"Pasirinkote testavimo opciją \n";
+    cout<<"Ką pageidaujate testuoti? \n  3 konstitucija.txt(efektyvumas) \n 4 lavinos efektas \n 5 atsparumas kolizijai  ";
+    cin>>pageidavimas;
+    switch(pageidavimas){
+      case 3:
+        konstitucijaTest();
+        break;
+      case 4:
+        avalanche_effectTest();
+        break;
+      case 5:
+        collision();
+        break;
+      default:
+        cout<<"Netinkama pasirinkimo reikšmė\n";
+    } 
+}
+
 int main()
 {
-    int user_answer=user_input_request();
-    if(user_answer==1)
-    {
-       openf_or_manual();
-    }
-    /* else{
-        
-    }   */
-
-    /* string hello= "fiF^Yy<].{9Lq6;kwGL7.8=#PB%QnmIA<v0$ZiaFjm,+paBveF";
-    string bin= asciiToBin(hello);
-    cout<<bin<<endl;
-  cout<<endl;
-    string padding=padding_of_bin(bin);
-    cout<<padding<<endl; 
-    cout<<endl;  */
-    
-   
-   /*  string gen;
-    cin>>gen; */
-   /*  generateF(gen, 1456); */
-
+  int user_answer=user_input_request();
+  if(user_answer==1)
+  {
+    openf_or_manual();
+  }
+  else{
+    test_option();
+}
 }
